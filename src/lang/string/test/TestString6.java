ac057574106c80076c97eb3c62c9987b030b1709
eggzidane <wousda@gmail.com>
2025-06-03 11:14:16 +0900

    3강, String 클래스의 자세한 정보 및 활용

---
+++ src/lang/string/test/TestString6.java
@@ -0,0 +1,19 @@
+package lang.string.test;
+
+public class TestString6 {
+
+    public static void main(String[] args) {
+        String str = "start hello java, hello spring, hello jpa";
+        String key = "hello";
+
+        int index = -1;
+        int count = 0;
+        while (true) {
+            index = str.indexOf(key, index + 1);
+            if (index == -1) break;
+            System.out.println("index = " + index);
+            count++;
+        }
+        System.out.println("count = " + count);
+    }
+}
